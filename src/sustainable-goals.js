c13d109c29b0723692852b112458308979914cae
djr23523 <141277951+djr23523@users.noreply.github.com>
2024-10-14 02:24:40 -0400

    Update sustainable-goals.js

--- src/sustainable-goals.js
+++ src/sustainable-goals.js
@@ -1,5 +1,28 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+const goalData = [
+  {
+    name: 'No Poverty',
+    color: '#e5243b',
+    image: new URL('../lib/svgs/goal-1.svg', import.meta.url).href,
+  },
+  { name: 'Zero Hunger', color: '#dda63a' },
+  { name: 'Good Health and Well-being', color: '#4c9f38' },
+  { name: 'Quality Education', color: '#c5192d' },
+  { name: 'Gender Equality', color: '#ff3a21' },
+  { name: 'Clean Water and Sanitation', color: '#26bde2' },
+  { name: 'Affordable and Clean Energy', color: '#fcc30b' },
+  { name: 'Decent Work and Economic Growth', color: '#a21942' },
+  { name: 'Industry, Innovation and Infrastructure', color: '#fd6925' },
+  { name: 'Reduced Inequalities', color: '#dd1367' },
+  { name: 'Sustainable Cities and Communities', color: '#fd9d24' },
+  { name: 'Responsible Consumption and Production', color: '#bf8b2e' },
+  { name: 'Climate Action', color: '#3f7e44' },
+  { name: 'Life Below Water', color: '#0a97d9' },
+  { name: 'Life on Land', color: '#56c02b' },
+  { name: 'Peace, Justice and Strong Institutions', color: '#00689d' },
+  { name: 'Partnerships for the Goals', color: '#19486a' },
+];
 
 export class sustainableGoals extends DDDSuper(LitElement) {
 
@@ -9,12 +32,24 @@ export class sustainableGoals extends DDDSuper(LitElement) {
 
   constructor() {
     super();
-    this.title = "";
+    this.goals="1";
+    this.label="";
+    this.alt=null;
+    this.colorOnly=false;
+    this._currentSrc=null;
+    this.loading="lazy";
+    this.fetchpriority="low";
   }
 
   static get properties() {
     return {
-      title: { type: String },
+      goals: {type: String, reflect: true},
+      colorOnly: {type: Boolean, attribute: "color-only", reflect: true},
+      label: {type:String},
+      _currentSrc: { type: String },
+      loading: {type: String},
+      fetchpriority: {type: String}
+
     };
   }
 
@@ -24,7 +59,9 @@ export class sustainableGoals extends DDDSuper(LitElement) {
       :host {
         display: block;
         color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
+        background-color: white;
+        width: 254px;
+        height: 254px;
         font-family: var(--ddd-font-navigation);
         font-size: var(--sustainable-goals-font-size, var(--ddd-font-size-s));
       }
@@ -35,6 +72,10 @@ export class sustainableGoals extends DDDSuper(LitElement) {
       div {
         padding: 0;
         margin: 0;
+      }
+      .color-only{
+        width:100%;
+        height:100%;
       }
     `];
   }
